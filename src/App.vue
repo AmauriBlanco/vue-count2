c428d47e9ecb5c4c7b4635807b0f944a9dad2ec5
AmauriBlanco <blancoamauri@gmail.com>
2024-07-15 19:56:52 -0300

    up

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref} from "vue"
+
+const count = ref(0)
+
+const addToCount = () => {
+    count.value++;
+  }
+
+  const subtractFromCount = () => {
+    count.value--;
+  }
+</script>
+
+
+<template>
+    <main>
+        <div>
+            <h4>The Curerent count is ...</h4>
+            <h1>
+                {{ count }}
+            </h1>
+            <button @click="subtractFromCount()">-</button>
+            <button @click="addToCount()">+</button>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    background-color: aquamarine;
+    widows: 100vw;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+}
+div {
+    text-align: center;
+}
+</style>
